b68bb381b76930dea0d4471a6f785ea7601b91d4
Shahabenawaz Alam <shahabe9558>
2023-11-22 14:08:39 +0530

    Html structure added

---
+++ index.js
@@ -0,0 +1,51 @@
+// API Key
+const API_KEY = "48394ddb7198d553ec33128fced2ead6";
+
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const searchForm = document.querySelector("[data-searchForm]");
+const userInfoContainer = document.querySelector(".userInfoContainer");
+const grantAccessContainer = document.querySelector(".grantLocationContainer");
+const loadingContainer = document.querySelector('.loadingContainer');
+
+const notFound = document.querySelector('.errorContainer');
+const errorBtn = document.querySelector('[data-errorButton]');
+const errorText = document.querySelector('[data-errorText]');
+const errorImage = document.querySelector('[data-errorImg]');
+
+let currentTab = userTab;
+currentTab.classList.add("currentTab");
+getFromSessionStorage();
+
+
+function switchTab(newTab) {
+    notFound.classList.remove("active");
+    // check if newTab is already selected or not 
+    if (currentTab != newTab) {
+        currentTab.classList.remove("currentTab");
+        currentTab = newTab;
+        currentTab.classList.add("currentTab");
+
+        // Check which TAb is Selected - search / your
+
+        // If Search Form not contains active class then add  [Search Weather]
+        if (!searchForm.classList.contains("active")) {
+            searchForm.classList.add("active");
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+        }
+        // Your Weather
+        else {
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            getFromSessionStorage();
+        }
+    }
+}
+userTab.addEventListener('click', () => {
+   switchTab(userTab);
+});
+
+searchTab.addEventListener('click', () => {
+   switchTab(searchTab);
+});
